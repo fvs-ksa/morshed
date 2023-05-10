51094a18118e82bbc2b6568d69221e238ea2548f
fvs-ksa <khalidadilalnajjar@gmail.com>
2023-05-10 17:49:46 +0300

    commit

--- lib/screen/inner_screen/another_services_screens/guides_screens/hijj_omra_info.dart
+++ lib/screen/inner_screen/another_services_screens/guides_screens/hijj_omra_info.dart
@@ -10,31 +10,47 @@ class HijAndOmraInfoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize=MediaQuery.of(context).size;
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-
-      appBar: headerForHijGuides(context: context, title:LocaleKeys.umrahAndHajjInformation.tr() , image: 'assets/images/umrah.png'),
+      appBar: headerForHijGuides(
+          context: context,
+          title: LocaleKeys.umrahAndHajjInformation.tr(),
+          image: 'assets/images/umrah.png'),
       body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: screenSize.width*0.03),
+        padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.03),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(children: [
-                SvgPicture.asset('assets/svg/Path 198320.svg'),
-                SizedBox(width: screenSize.width*0.03,),
-                Text('الحج')
-              ],),
-              Text('لوريم إيبسوم(Lorem Ipsum) هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع'
-                  ' من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" '
-                  '(Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.',style: Theme.of(context).textTheme.bodySmall,),
-              Row(children: [
-                SvgPicture.asset('assets/svg/Path 198320.svg'),
-                SizedBox(width: screenSize.width*0.03,),
-                Text('العمره')
-              ],),
-              Text('لوريم إيبسوم(Lorem Ipsum) هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع'
-                  ' من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" '
-                  '(Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.',style: Theme.of(context).textTheme.bodySmall,)
+              Row(
+                children: [
+                  SvgPicture.asset('assets/svg/Path 198320.svg'),
+                  SizedBox(
+                    width: screenSize.width * 0.03,
+                  ),
+                  Text('الحج')
+                ],
+              ),
+              Text(
+                'لوريم إيبسوم(Lorem Ipsum) هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع'
+                ' من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" '
+                '(Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              Row(
+                children: [
+                  SvgPicture.asset('assets/svg/Path 198320.svg'),
+                  SizedBox(
+                    width: screenSize.width * 0.03,
+                  ),
+                  Text('العمره')
+                ],
+              ),
+              Text(
+                'لوريم إيبسوم(Lorem Ipsum) هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي منذ القرن الخامس عشر عندما قامت مطبعة مجهولة برص مجموعة من الأحرف بشكل عشوائي أخذتها من نص، لتكوّن كتيّب بمثابة دليل أو مرجع شكلي لهذه الأحرف. خمسة قرون من الزمن لم تقضي على هذا النص، بل انه حتى صار مستخدماً وبشكله الأصلي في الطباعة والتنضيد الإلكتروني. انتشر بشكل كبير في ستينيّات هذا القرن مع إصدار رقائق "ليتراسيت" (Letraset) البلاستيكية تحوي مقاطع'
+                ' من هذا النص، وعاد لينتشر مرة أخرى مؤخراَ مع ظهور برامج النشر الإلكتروني مثل "ألدوس بايج مايكر" '
+                '(Aldus PageMaker) والتي حوت أيضاً على نسخ من نص لوريم إيبسوم.',
+                style: Theme.of(context).textTheme.bodySmall,
+              )
             ],
           ),
         ),
